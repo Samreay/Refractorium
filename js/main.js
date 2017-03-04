6bfb1630a18a1f756176e436af86a015cd79e3f7
samreay <samuelreay@gmail.com>
2017-03-04 22:02:28 +1000

    Cool pattern

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ scene.addObject(new Line(0.1, 0.30, 0.30, 0.10, 0.0, 0.0, 0.0));
 scene.addObject(new Box(0.3, 0.1, 0.2, 0.2, 0.2, 0.0, 0.0, 1.7, 0.0));
 scene.addObject(new Cylinder(0.7, 0.58, 0.08, 0.0, 0.0, 1.5, 0.0));
 
-scene.addObject(new Cylinder(0.1, 0.50, 0.1, 0.0, 0.2, 1.2, 0.));
+scene.addObject(new Cylinder(0.15, 0.50, 0.14, 0.0, 0.2, 1.2, 0.));
 
 // scene.addObject(new Prism(0.7, 0.55, 0.2, 0.2, 3.14, 0.0, 0.0, 1.2, 0.0));
-scene.addObject(new Prism(0.3, 0.55, 0.2, 0.2, 3.14, 0.0, 0.0, 1.2, 0.0));
+scene.addObject(new Prism(0.4, 0.58, 0.2, 0.2, 3.14, 0.0, 0.0, 1.2, 0.0));
 
 // scene.addObject(new Cylinder(0.4, 0.50, 0.3, 0.0, 0.3, 1.1, 0.0));
 
