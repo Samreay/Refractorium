d4eac3f23e38149be69c9f9af5e637e23ce90713
samreay <samuelreay@gmail.com>
2017-03-05 23:05:47 +1000

    Better initial

--- js/scenes.js
+++ js/scenes.js
@@ -14,18 +14,13 @@ angular.module('servicesZ', []).service("scenesService", [function () {
     scene.addObject(new Prism(0.34, 0.5, 0.12, Math.PI/2, 0.0, 0.0, 1.2, 0.0));
     scene.addObject(new Cylinder(0.7, 0.58, 0.1, 0.0, 0.0, 1.5, 0.0));
 
-    scene.addObject(new Line(0.32, 0.63, 2.15, 0.05, 0.0, 0.1, 0.0));
-    scene.addObject(new Line(1.3, 0.4,  -0.8, 0.55, 0.0, 1, 0.01));
-    scene.addObject(new Line(1.48, 0.63,  Math.PI/2, 0.5, 0.0, 1, 1));
+    scene.addObject(new Line(0.32, 0.63, 2.15, 0.05, 0.1, 0.1, 0.0));
+    scene.addObject(new Line(1.3, 0.4,  -0.8, 0.55, 0.1, 1, 0.01));
+    scene.addObject(new Line(1.48, 0.63,  Math.PI/2, 0.5, 0.1, 1, 1));
 
-    // scene.addObject(new Line(1.3, 0.20, 1.0, 0.60, 0.0, 1.0, 1.0));
-    // scene.addObject(new Line(1.4, 0.20, 1.4, 0.80, 0.0, 1.0, 1.0));
-    // scene.addObject(new Line(0.1, 0.30, 0.30, 0.10, 0.0, 1.0, 0.0));
-    // scene.addObject(new Box(0.5, 0.01, 0.2, 0.2, 0.1, 0.0, 0.0, 1.7, 0.0));
-    // scene.addObject(new Cylinder(0.15, 0.50, 0.14, 0.0, 0.2, 1.2, 0.));
-    // scene.addObject(new Line(0.38, 0.4, 0.38, 0.51, 0.0, 0.5, 0.001));
-    // scene.addObject(new Line(1, 0.64, 1, 0.66, 0.0, 1.0, 0.7));
-    // scene.addObject(new ConvexLens(0.9, 0.4, 0.2, 1.1, 0.8, 0, 0, 1.5, 0));
+    scene.addObject(new Line(1.33, 0.75, 0.6, 0.4, 0.1, 1.0, 0.5));
+    scene.addObject(new ConvexLens(1.1, 0.43, 0.2, 0.2, 0.9, 0, 0, 1.5, 0));
+
     scenes.push({"name": "Complicated", "scene": scene});
 
 
